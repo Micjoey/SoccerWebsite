efd6a0311190ed61d4a1d72bc1e48ba4724affd3
Macallan Savett <jsavett@gmail.com>
2023-11-16 18:00:31 -0800

    refactor(PULL_REQUEST_TEMPLATE.md): set file name\nApp.tsx;HomePage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
-
 import SchedulePage from "./components/SchedulePage/SchedulePage";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/schedule" element={<SchedulePage />} />{" "}
