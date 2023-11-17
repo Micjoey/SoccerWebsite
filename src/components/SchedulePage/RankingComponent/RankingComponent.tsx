5adeddafdda6ba8b8f75ba24e1dc2adcaabc236d
Macallan Savett <jsavett@gmail.com>
2023-11-16 19:26:04 -0800

    Feature(Ranking): Setup ranking data on page\nsoccerRanking.js;RankingComponent.tsx

--- src/components/SchedulePage/RankingComponent/RankingComponent.tsx
+++ src/components/SchedulePage/RankingComponent/RankingComponent.tsx
@@ -1,9 +1,34 @@
-import React from "react";
-import "./RankingComponent.scss";
+// RankingComponent.js
+import GenericTableComponent from "../../Utils/GenericTableComponent";
+import useFetchData from "../../Utils/useFetchData";
 
-const RankingComponent: React.FC = () => {
+const RankingComponent = () => {
+  const { data, loading, error } = useFetchData(
+    "http://localhost:3001/ranking",
+  );
+
+  const columns = [
+    { header: "Team Name", accessor: "teamName" },
+    { header: "Games Played", accessor: "gamesPlayed" },
+    { header: "Wins", accessor: "wins" },
+    { header: "Losses", accessor: "losses" },
+    { header: "Ties", accessor: "ties" },
+    { header: "Goals For", accessor: "goalsFor" },
+    { header: "Goals Against", accessor: "goalsAgainst" },
+    { header: "Goal Differential", accessor: "goalDifferential" },
+    { header: "Points", accessor: "points" },
+  ];
+
   return (
-    <div className="RankingComponent">{/* Component content goes here */}</div>
+    <div>
+      <h1>Team Rankings</h1>
+      <GenericTableComponent
+        data={data}
+        columns={columns}
+        loading={loading}
+        error={error}
+      />
+    </div>
   );
 };
 
