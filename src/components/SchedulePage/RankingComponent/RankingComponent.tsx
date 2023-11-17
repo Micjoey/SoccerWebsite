77084a677f8008c782f0df452037761877bdfc69
Macallan Savett <jsavett@gmail.com>
2023-11-16 17:48:04 -0800

    Feature(RankingComponent): Setup Shell of ranking component\nRankingComponent.scss;RankingComponent.tsx

---
+++ src/components/SchedulePage/RankingComponent/RankingComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./RankingComponent.scss";
+
+const RankingComponent: React.FC = () => {
+  return (
+    <div className="RankingComponent">{/* Component content goes here */}</div>
+  );
+};
+
+export default RankingComponent;
