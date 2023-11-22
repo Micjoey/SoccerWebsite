9d2d4e4428fe36af0f1e1816b5d2866414bad20e
Macallan Savett <jsavett@gmail.com>
2023-11-21 21:26:00 -0800

    Feature(docker): attempt to setup docker\nDockerfile;docker-compose.yml;Dockerfile.postgres;dbConfig.js;docker-entrypoint.sh;package-lock.json;package.json;server.js

--- soccer-team-backend/src/server.js
+++ soccer-team-backend/src/server.js
@@ -1,7 +1,17 @@
+import { pool } from "../db/dbConfig.js";
 import app from "./app.js"; // Import the configured express app
 
 const port = process.env.PORT || 3001; // Use environment port or default to 3001
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+
+  pool.query("SELECT version()", (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(res.rows[0]);
+    }
+    pool.end();
+  });
 });
