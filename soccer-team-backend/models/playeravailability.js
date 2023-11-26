3059447b34e6422fefe9a5c9737d65a1f77714ed
Macallan Savett <jsavett@gmail.com>
2023-11-26 13:35:07 -0800

    Feature(Player Availability): Create availability model\n20231126212217-create-player-availability.js;playeravailability.js

---
+++ soccer-team-backend/models/playeravailability.js
@@ -0,0 +1,30 @@
+"use strict";
+import { Model } from "sequelize";
+export default (sequelize, DataTypes) => {
+  class PlayerAvailability extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Relationship with Player
+      PlayerAvailability.belongsTo(models.Player, { foreignKey: "playerId" });
+
+      // Relationship with Game
+      PlayerAvailability.belongsTo(models.Game, { foreignKey: "gameId" });
+    }
+  }
+  PlayerAvailability.init(
+    {
+      playerId: DataTypes.INTEGER,
+      gameId: DataTypes.INTEGER,
+      availability: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "PlayerAvailability",
+    },
+  );
+  return PlayerAvailability;
+};
