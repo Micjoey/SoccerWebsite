18d94eead369f2e4d587cff859d0fcbdcbe3496e
Macallan Savett <jsavett@gmail.com>
2023-11-26 13:37:04 -0800

    Feature(Admintask): Create admin task model\n20231126212225-create-admin-task.js;admintask.js

---
+++ soccer-team-backend/models/admintask.js
@@ -0,0 +1,27 @@
+"use strict";
+import { Model } from "sequelize";
+export default (sequelize, DataTypes) => {
+  class AdminTask extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Relationship with User
+      AdminTask.belongsTo(models.User, { foreignKey: "userId" });
+    }
+  }
+  AdminTask.init(
+    {
+      userId: DataTypes.INTEGER,
+      task: DataTypes.TEXT,
+      status: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "AdminTask",
+    },
+  );
+  return AdminTask;
+};
