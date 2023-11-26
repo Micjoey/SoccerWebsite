ab15bc4e409a94ae9872c2357ef80625f62048af
Macallan Savett <jsavett@gmail.com>
2023-11-26 13:36:36 -0800

    Feature(Gameresult): create game result model\n20231126212222-create-game-result.js;gameresult.js

---
+++ soccer-team-backend/models/gameresult.js
@@ -0,0 +1,27 @@
+"use strict";
+import { Model } from "sequelize";
+export default (sequelize, DataTypes) => {
+  class GameResult extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Relationship with Game
+      GameResult.belongsTo(models.Game, { foreignKey: "gameId" });
+    }
+  }
+  GameResult.init(
+    {
+      gameId: DataTypes.INTEGER,
+      score: DataTypes.STRING,
+      summary: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "GameResult",
+    },
+  );
+  return GameResult;
+};
