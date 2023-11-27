b9b1ff226db05c57668cf101e2e959e5ad8534fb
Macallan Savett <jsavett@gmail.com>
2023-11-26 18:24:32 -0800

    Feature(routeHandler.js): Simply routes for now

--- soccer-team-backend/src/routeHandler.js
+++ soccer-team-backend/src/routeHandler.js
@@ -3,13 +3,11 @@
 import scheduleRouter from "./routes/scheduleRouter.js";
 import rankingRouter from "./routes/rankingRouter.js";
 import gameRouter from "./routes/gameRouter.js";
-import adminTaskRouter from "./routes/adminTaskRouter.js";
 
 const setupRoutes = (app) => {
   app.use("/", scheduleRouter);
   app.use("/", rankingRouter);
   app.use("/games", gameRouter);
-  app.use("/admintasks", adminTaskRouter);
 };
 
 export default setupRoutes;
